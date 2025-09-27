8876d0d837ea5ed6ab2c23238534513554721d14
Anderzon Bautista <bautistaanderzon@gmail.com>
2025-09-27 09:26:06 -0400

    Add Authentication

---
+++ GestorDocumentoApp/Controllers/AccountController.cs
@@ -0,0 +1,62 @@
+﻿using GestorDocumentoApp.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestorDocumentoApp.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(SignInManager<IdentityUser> signInManager)
+        {
+            _signInManager = signInManager;
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string? returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View(); ;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginVM loginVm, string? returnUrl = null)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(loginVm);
+                }
+
+                var result = await _signInManager.PasswordSignInAsync(loginVm.Email, loginVm.Password, loginVm.RememberMe, lockoutOnFailure: false);
+
+                if (result.Succeeded)
+                {
+                    return LocalRedirect(returnUrl ?? "/");
+                }
+
+                ModelState.AddModelError(string.Empty, "Credenciales invalidas.");
+                return View(loginVm);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Error en el servidor.");
+                return View(loginVm);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction(nameof(Login));
+
+        }
+
+    }
+}
+
--- GestorDocumentoApp/Program.cs
+++ GestorDocumentoApp/Program.cs
@@ -1,9 +1,45 @@
+using GestorDocumentoApp.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<ScmDocumentContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddIdentity<IdentityUser,IdentityRole>()
+    .AddEntityFrameworkStores<ScmDocumentContext>()
+    .AddDefaultTokenProviders();
+
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.AccessDeniedPath = "/Account/AccessDenied";
+    });
+
+builder.Services.AddAuthorization(opt =>
+{
+    opt.FallbackPolicy = opt.DefaultPolicy;
+});
+
 var app = builder.Build();
+
+
+//using (var scope = app.Services.CreateScope())
+//{
+//    var services= scope.ServiceProvider;
+//    var userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+//    var roleManage = services.GetRequiredService<RoleManager<IdentityRole>>();
+//    await DbSeeder.SeedAsync(userManager, roleManage, app.Configuration);
+//}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -14,9 +50,13 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
+
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapStaticAssets();
 
