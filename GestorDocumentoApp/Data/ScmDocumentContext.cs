96c901fdf13c26594eafafcdb57020c9b40f3635
Anderzon-cmd <66657765+Anderzon-cmd@users.noreply.github.com>
2025-09-27 10:04:52 -0400

    feat : add authentication

---
+++ GestorDocumentoApp/Data/ScmDocumentContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestorDocumentoApp.Data
+{
+    public class ScmDocumentContext : IdentityDbContext<IdentityUser>
+    {
+        public ScmDocumentContext(DbContextOptions<ScmDocumentContext> options) : base(options)
+        {
+        }
+
+        public ScmDocumentContext()
+        {
+
+        }
+
+        
+    }
+}
